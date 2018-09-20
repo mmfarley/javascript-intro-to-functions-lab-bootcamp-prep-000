5650e263e790000dca6a41f88a029ad08284596c
Megan Farley <megfarley4@gmail.com>
2018-09-20 02:01:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string){
   if (string === "HELLO!"){
     return "YES INDEED!"
   }
-  if (string === "hello!"){
-    return "I can't hear you!"
+  if (string === "hello"){
+    return "I can\'t hear you!"
   }
   if (string === "I love you, Grandma."){
     return "I love you, too."
